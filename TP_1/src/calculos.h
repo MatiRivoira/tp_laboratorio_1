f2ee6c49aee56b4ab02dfb1edbd2fcd1b04ec03a
MatiRivoira <98726423+MatiRivoira@users.noreply.github.com>
2022-04-14 18:50:35 -0300

    TRABAJO TERMINADO 1.0
    FALTARIA HACER LOS PRIMERO PRINT EN FUNCION :C, LAMENTABLEMENTE NO ME DEJA

---
+++ TP_1/src/calculos.h
@@ -0,0 +1,16 @@
+/*
+ * calculos.h
+ *
+ *  Created on: 12 abr. 2022
+ *      Author: Rivoira
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+
+int calculosLatam(int precioLatam, int precioAereolineas, int kilometros, float* pTarjetaCredito, float* pTarjetaDebito, float* pBitcoin, float* pPrecioUnitario, float* pDiferenciaPrecio);
+
+int calculosAereolineas(int precioLatam, int precioAereolineas, int kilometros, float* pTarjetaCredito, float* pTarjetaDebito, float* pBitcoin, float* pPrecioUnitario);
+
+
+#endif /* CALCULOS_H_ */
